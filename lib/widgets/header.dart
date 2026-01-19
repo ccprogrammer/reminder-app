bc4d2c059b198c823bc46fa8b73d92e354fff6c3
Lubianca Samora <lubianca.samora@gmail.com>
2026-01-19 09:42:37 +0700

    update

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reminder_app/bloc/reminder_bloc.dart';
 
 class AppHeader extends StatelessWidget {
   const AppHeader({super.key});
@@ -7,24 +9,52 @@ class AppHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
+      children: [
         Text(
           "Hello,",
           style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
         ),
-        Text.rich(
-          TextSpan(
-            children: [
+        BlocBuilder<ReminderBloc, ReminderState>(
+          builder: (context, state) {
+            if (state is ReminderLoaded && state.reminders.isNotEmpty) {
+              final reminders = state.reminders;
+
+              return Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '${reminders.length} Tasks ',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'today',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return Text.rich(
               TextSpan(
-                text: '4 Tasks ',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                    text: 'What\'s new ',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: 'today?',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                  ),
+                ],
               ),
-              TextSpan(
-                text: 'today',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ],
     );
